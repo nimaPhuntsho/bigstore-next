43e39ce244672414f00f2a423edc27d3fe0af3c7
nimaPhuntsho <nimafunso@gmail.com>
2025-03-07 16:09:40 +1000

    deploy fix

--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -1,6 +1,6 @@
 import ForgotPasswordForm from "@/components/custom/ForgotPasswordForm";
 import { Button, Heading, VStack } from "@chakra-ui/react";
 
-export default async function () {
+export default async function ResetPassword() {
   return <ForgotPasswordForm />;
 }
