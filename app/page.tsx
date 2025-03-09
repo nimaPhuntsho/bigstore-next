0036e007cae1be1da30fe922587c5118a04ec188
nimaPhuntsho <nimafunso@gmail.com>
2025-03-09 22:54:21 +1000

    edited landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,63 @@
-import { Heading, VStack, Text } from "@chakra-ui/react";
+import {
+  Heading,
+  VStack,
+  Text,
+  HStack,
+  Flex,
+  Box,
+  Button,
+} from "@chakra-ui/react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <VStack>
-        <VStack
-          width={{
-            base: "90%",
-            sm: "500px",
-          }}
-          alignItems="center"
-          justifyContent="center"
-          height="100dvh"
-        >
-          <Heading fontWeight={800} textAlign="center">
-            E-Commerce Website with Next.js & Supabase
+      <VStack
+        p="2rem"
+        minH="100dvh"
+        alignItems="center"
+        justifyContent="center"
+        gap="2rem"
+      >
+        <VStack>
+          <Heading
+            fontWeight={800}
+            size={{
+              base: "4xl",
+              md: "5xl",
+            }}
+            textAlign="center"
+          >
+            Your one-stop shop for everything you love. Explore, shop, enjoy!
           </Heading>
-          <Text textAlign="justify">
-            Built a full-featured e-commerce platform with Next.js and Supabase,
-            featuring secure authentication, password reset, order management,
-            and a seamless shopping experience.
+          <Text fontWeight={400} textAlign="center">
+            Discover the perfect products just for you. Shop now and find what
+            you’ve been looking for
           </Text>
         </VStack>
+        <Link href="/listings">
+          <Button
+            _active={{
+              bgColor: "#F7F7F7",
+              color: "black",
+              transform: "scale(0.95)",
+            }}
+            transition="all .1s ease-in-out"
+            fontWeight={700}
+            size="lg"
+          >
+            Get Started
+          </Button>
+        </Link>
+        <Box>
+          <Image
+            src="/images/landing-img.png"
+            width={700}
+            height={700}
+            alt="landing image"
+          />
+        </Box>
       </VStack>
     </>
   );
